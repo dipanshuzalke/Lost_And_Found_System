5c2181210cee015c60931465fc77b95856577721
soham <pisesoham19@gmail.com>
2024-08-14 17:08:12 +0530

    swiched to mongo db from simple array

---
+++ init/data.js
@@ -0,0 +1,7 @@
+const sampleData = [{
+    item: "college bag",
+    description : "the bag has some notebooks, a history text-book and a pencil box",
+    question: "What is the color of the bag?",
+}
+];
+module.exports = { data: sampleData };
